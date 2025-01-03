333cc9a2c20e54e79b2af8612f735e850fcd0865
Engineer <Engineer@DESKTOP-2AC9G10>
2025-01-03 18:03:23 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Merhaba Dünya");
+            //Console.Write("Selam"); 
+
+            Console.WriteLine("****** Yemek Kategorileri *******");
+            Console.WriteLine();
+            Console.WriteLine("1-Çorbalar");
+            Console.WriteLine("1-Ana Yemekler");
+            Console.WriteLine("1-Soğuk Başlangıçlar");
+            Console.WriteLine("1-Salatalar");
+            Console.WriteLine("1-Tatlılar");
+            Console.WriteLine("1-İçecekler");
+                
+
+            Console.Read();
+        }
+    }
+}
